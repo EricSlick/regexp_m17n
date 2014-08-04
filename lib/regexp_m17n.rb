9f6651e6bedf30c791f0e3d9d8fc0f8beaf976fe
Eric Slick <github@slickfamily.net>
2014-08-03 19:30:52 -0700

    PUG BOMB!
    - change default encoding? WTF?

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,9 +1,9 @@
 module RegexpM17N
-  def self.non_empty?(str)
-    str =~ /^.+$/
+  def self.non_empty?(str, enc)
+    Regexp.new('^.+$'.encode(enc)).match(str.encode(enc))
   end
 
   def self.non_empty2?(str)
-    Regexp.new('^.+$'.encode(str.encoding)).match(str)
+
   end
 end
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -4,12 +4,9 @@ require_relative '../lib/regexp_m17n'
 
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
-    default_encoding = Encoding.default_internal
     Encoding.list.each do |enc|
-      Encoding.default_internal = enc
-      assert RegexpM17N.non_empty?('.')
+      assert RegexpM17N.non_empty?('.', enc) unless enc.dummy?
     end
-    Encoding.default_internal = default_encoding
   end
 
 end
