1118fe7f22b95e1b52e96fef921b65daa5e55231
Eric Slick <github@slickfamily.net>
2014-08-05 20:24:58 -0700

    Find an ascii compatible converter to help deal with some stateless encodings
     - utf-7 and jp-2 are still being pains in the arse

--- lib/regexp_m17n.rb
+++ lib/regexp_m17n.rb
@@ -1,6 +1,15 @@
 module RegexpM17N
   def self.non_empty?(str, enc)
-    Regexp.new('^.+$'.encode(enc)).match(str.encode(enc))
+    unless enc.dummy?
+      Regexp.new('^.+$'.encode(enc)).match(str.encode(enc))
+    else
+
+      if conv = Encoding::Converter.asciicompat_encoding(enc)
+        Regexp.new('^.+$'.encode(conv)).match(str.encode(conv))
+      else
+        111
+      end
+    end
   end
 
   def self.non_empty2?(str)
--- test/regexp_m17n_test.rb
+++ test/regexp_m17n_test.rb
@@ -5,7 +5,8 @@ require_relative '../lib/regexp_m17n'
 class RegexpTest < MiniTest::Unit::TestCase
   def test_non_empty_string
     Encoding.list.each do |enc|
-      assert RegexpM17N.non_empty?('.', enc) unless enc.dummy?
+      result = RegexpM17N.non_empty?('.', enc)
+      assert(result)  unless result == 111
     end
   end
 
